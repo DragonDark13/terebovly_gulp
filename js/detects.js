1f910145f3c664e3de141ae053ca2d447c38b640
Aleksandr <aleksdark13@gmail.com>
2024-08-29 18:34:51 +0300

    Updates

---
+++ js/detects.js
@@ -0,0 +1,7 @@
+/*! BuildToolsCookbook v2.0.0 | (c) 2024 Your Name | MIT License | http://github.com/cferdinandi/build-tools-boilerplate */
+(function () {
+	'use strict';
+
+	console.log('detects.js file loaded');
+
+})();
